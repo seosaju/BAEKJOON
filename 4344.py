2f18bb5eee2e0fa229cb5c9bf8896076386e0789
seosaju <qua_ver@naver.com>
2018-04-06 15:04:47 +0900

    평균은 넘겠지

--- 4344.py
+++ 4344.py
@@ -3,5 +3,5 @@ x = list(list(map(int, input().split())) for _ in range(n))
 
 for i in range(n):
     avg = sum(x[i][1:]) / x[i][0]
-    over_avg = [str(n) for n in map(int, x[i][1:]) if n > avg]
+    over_avg = [n for n in x[i][1:] if n > avg]
     print("{:.3f}".format(len(over_avg) / (len(x[i]) - 1) * 100) + "%")

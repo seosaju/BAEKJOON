13f58bb60cd968c748717e4ec3483a49bce614a1
seosaju <qua_ver@naver.com>
2018-06-20 11:08:36 +0900

    2007년

--- 1924.py
+++ 1924.py
@@ -2,7 +2,7 @@ month_list = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_list = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 day_sum = 0
 
-month, day = map(int, input().split(" "))
+month, day = map(int, input().split())
 for i in range(month):
     day_sum += month_list[i]
 day_sum += day

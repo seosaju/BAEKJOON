ecce82a423f9d295ba0595aaa1046d7e1fa53cd9
seosaju <qua_ver@naver.com>
2018-06-20 14:50:00 +0900

    별 찍기 - 11

---
+++ 2448.py
@@ -0,0 +1,21 @@
+import math
+
+s = ["  *   ", " * *  ", "***** "]
+
+
+def make_star(shift):
+    c = len(s)
+    for i in range(c):
+        s.append(s[i] + s[i])
+        s[i] = ("   " * shift + s[i] + "   " * shift)
+
+
+n = int(input())
+k = int(math.log(int(n / 3), 2))
+for i in range(k):
+    make_star(int(pow(2, i)))
+
+for i in range(n):
+    print(s[i])
+
+# 출처: http://lazineer.tistory.com/179

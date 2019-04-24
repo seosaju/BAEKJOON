bba4c19b03f43ad0495a0acabd2270029a2e6f36
seosaju <qua_ver@naver.com>
2019-04-24 16:13:34 +0900

    터렛

--- 1002.py
+++ 1002.py
@@ -1,20 +1,15 @@
 import sys
 
-n = int(sys.stdin.readline())
-for _ in range(n):
+for _ in range(int(sys.stdin.readline())):
     x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
     distance = abs(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
-    sqrt_sum = r1 + r2
-    sqrt_sub = abs(r1 - r2)
+    sum_r = r1 + r2
+    sub_r = abs(r1 - r2)
     if distance == 0 and r1 == r2:
         print(-1)
-        continue
-    if sqrt_sum < distance or distance < sqrt_sub:
+    elif sum_r < distance or distance < sub_r:
         print(0)
-        continue
-    if distance == sqrt_sum or distance == sqrt_sub:
+    elif distance == sum_r or distance == sub_r:
         print(1)
-        continue
     else:
         print(2)
-

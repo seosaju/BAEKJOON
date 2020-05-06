80db8070f435a3de963e6c14b402314851e9bebb
seosaju <qua_ver@naver.com>
2020-05-06 15:19:13 +0900

    오르막 수

--- 11057.py
+++ 11057.py
@@ -1,9 +1,7 @@
-n = int(input())
 dp = [[], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
-for i in range(2, n + 1):
-    tmp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for digit in range(10):
-        tmp[digit] += sum(dp[i - 1][digit:])
+for i in range(2, int(input()) + 1):
+    tmp = []
+    for j in range(10):
+        tmp.append(sum(dp[i-1][j:]))
     dp.append(tmp)
-print(sum(dp[n]) % 10007)
+print(sum(dp[-1]) % 10007)

513243790c75ccc91c2abbc987450802259815d9
seosaju <qua_ver@naver.com>
2020-05-06 17:25:24 +0900

    가장 긴 감소하는 부분 수열

---
+++ 11722.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] < arr[j] and dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
+print(max(dp))

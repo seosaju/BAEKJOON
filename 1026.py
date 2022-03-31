1088cf67157895a6fb3a0b0feda94c0ebd36dfc3
seosaju <qua_ver@naver.com>
2022-03-31 12:26:04 +0900

    보물

---
+++ 1026.py
@@ -0,0 +1,13 @@
+n = int(input())
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+
+a_list.sort()
+b_list.sort()
+b_list.reverse()
+
+result = 0
+for i in range(n):
+    result += a_list[i] * b_list[i]
+
+print(result)
